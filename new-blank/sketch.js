ccb208f804aa426a1e1316ec7f962110445f1628
Ben Parsons <ben@bpulse.co.uk>
2016-08-16 21:18:56 +0100

    easy little blank project

---
+++ new-blank/sketch.js
@@ -0,0 +1,16 @@
+var canvasWidth = 600,
+  canvasHeight = 600;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  noStroke();
+  //noLoop();
+
+  //background(153);
+}
+
+function draw() {
+  // rect(leftDistance, topDistance, width, height)
+  // fill(r, g, b); fill(grey); noFill();
+  // map(value, in-range-min, in-range-max, out-range-min, out-range-max)
+}
