c17429bddcadecdca88187d28436a5ec4d1b63d6
Ben Parsons <ben@bpulse.co.uk>
2016-08-16 21:13:04 +0100

    gitchboxes

---
+++ glitchboxes/sketch.js
@@ -0,0 +1,42 @@
+// this sketch was inspired by the pillars in this comic:
+// http://www.earthexplodes.com/comics/134/
+
+
+var frame = 0;
+var maxFrames = 10;
+
+var xBoxCount = 30;
+var yBoxCount = 30;
+
+var canvasWidth = 600,
+  canvasHeight = 600;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  noStroke();
+  //noLoop();
+
+  //background(153);
+
+}
+
+function draw() {
+
+  var boxWidth = canvasWidth / xBoxCount;
+  var boxHeight = canvasHeight / yBoxCount;
+
+  for (var i = 0; i < xBoxCount; i++) {
+    for (var j = 0; j < xBoxCount; j++) {
+      var r = map(i, 0, xBoxCount, 0, 255);
+      var g = map(j, 0, yBoxCount, 0, 255);
+      fill(r, g, 255);
+      // leftDistance, topDistance, width, height
+      rect(i * boxWidth, j * boxHeight, boxWidth, boxHeight);
+    }
+  }
+
+  xBoxCount = round(random(100));
+  yBoxCount = round(random(100));
+  //if (xBoxCount < 1 || yBoxCount < 1) noLoop();
+
+}
