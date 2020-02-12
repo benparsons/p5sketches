7d0ec23a3c4274eea562311cf9e2860f9846979e
Ben Parsons <ben@bpulse.co.uk>
2020-02-12 09:39:35 +0000

    2020-02-11-blend-play

---
+++ 2020-02-11-blend-play/sketch.js
@@ -0,0 +1,26 @@
+var images = [];
+var index = 0;
+
+function preload() {
+  for (var i = 1; i <= 9; i++) {
+    images.push(loadImage(i+'.jpg'));
+  }
+}
+
+
+function setup() {
+  createCanvas(640, 480);
+  blendMode(SUBTRACT);
+  //frameRate(2)
+}
+
+function draw() {
+  if (index>8) {
+    blendMode(LIGHTEST);
+    index = 0;
+    background(255);
+    blendMode(SUBTRACT);
+  }
+  image(images[index], 0, 0);
+  index++;
+}
