809b5fafac6bc69650bb984f8696156d4582e35d
Ben Parsons <ben@bpulse.co.uk>
2020-05-11 23:26:47 +0100

    a better understanding of blendMode DIFFERENCE

---
+++ 2020-05-11/sketch3.js
@@ -0,0 +1,36 @@
+// from 2020-02-11-blend-play
+
+var images = [];
+var index = 0;
+
+function preload() {
+  for (var i = 1; i <= 9; i++) {
+    images.push(loadImage(`../images/bright-run/${i}.jpg`));
+  }
+}
+
+
+function setup() {
+
+  createCanvas(1280, 480);
+  image(images[0], 640, 0)
+  frameRate(5)
+  //noLoop();
+}
+
+function draw() {
+
+  if (index+1 >= images.length) {
+      blendMode(BLEND);
+      image(images[0], 0, 0);
+      image(images[0], 640, 0)
+      index = 1;
+      return;
+  }
+  blendMode(DIFFERENCE);
+
+  image(images[index], 0, 0);
+  index++;
+  image(images[index], 0, 0);
+  index++;
+}
