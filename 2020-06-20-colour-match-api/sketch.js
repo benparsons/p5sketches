e104528c206185abfd31083e18a78ce19a323185
Ben Parsons <ben@bpulse.co.uk>
2020-06-20 13:45:14 +0100

    colour-match-api

---
+++ 2020-06-20-colour-match-api/sketch.js
@@ -0,0 +1,85 @@
+var canvasWidth = 1280,
+  canvasHeight = 400;
+var img, from, to;
+let steps = 20;
+let colors = [];
+let imageWidth = 2560 / 4 / 1;
+let imageHeight = 1600 / 4 / 1;
+
+function setup() {
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.position(0,0);
+
+  let buttonYes = createButton('YES');
+  buttonYes.position(canvasWidth + 10, canvasHeight / 2);
+  buttonYes.mousePressed(() => clickYes(from, to));
+
+  let buttonNo = createButton('NO');
+  buttonNo.position(canvasWidth + 10, canvasHeight / 2 + 20);
+  buttonNo.mousePressed(() => renderFresh());
+
+  noLoop();
+
+  loadNewImage();
+}
+
+function loadSourceImage() {
+  img.resize(imageWidth, imageHeight);
+  image(img, 0, 0, imageWidth, imageHeight);
+
+  renderFresh();
+}
+
+function renderFresh() {
+  from = color(
+    int(random(256)),
+    int(random(256)),
+    int(random(256)));
+  to = color(
+    int(random(256)),
+    int(random(256)),
+    int(random(256)));
+
+  colors = [];
+  for (var s = 0; s < steps; s++) {
+    let lerpVal = (1 / steps) * (steps - s);
+    colors.push(lerpColor(from, to, lerpVal));
+  }
+  
+  for (var x = 0; x < imageWidth; x++) {
+    for (var y = 0; y < imageHeight; y++) {
+      var c = img.get(x, y);
+      stroke(getNearest(c));
+      
+      point(x + 640, y);
+    }
+  }
+}
+
+function getNearest(c) {
+  var dist = Infinity;
+  var result;
+  for (test of colors) {
+    var testDist = 
+      Math.abs(test.levels[0]-c[0]) +
+      Math.abs(test.levels[1]-c[1]) +
+      Math.abs(test.levels[2]-c[2]);
+    if (testDist < dist) {
+      dist = testDist;
+      result = test;
+    }
+  }
+  return result;
+}
+
+function clickYes(from, to) {
+  console.log(from.levels, to.levels);
+  renderFresh();
+}
+
+function loadNewImage() {
+  let url = "http://localhost:8090/api/1/get/scene";
+  loadJSON(url, data => {
+    img =loadImage(data.url, loadSourceImage);
+  });
+}
