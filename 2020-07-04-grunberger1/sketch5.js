493347e38bd4429082b83dac19716c89742ce9e5
Ben Parsons <ben@bpulse.co.uk>
2020-07-04 16:52:28 +0100

    draw per element

--- 2020-07-04-grunberger1/sketch5.js
+++ 2020-07-04-grunberger1/sketch5.js
@@ -4,6 +4,7 @@ let vectors = [];
 let vectorCount = 3;
 let backgroundColor;
 let elements = 8;
+let x = 0, y = 0;
 
 var canvasWidth = gridWidth * scale * elements,
   canvasHeight = gridHeight * scale * elements;
@@ -11,20 +12,31 @@ var canvasWidth = gridWidth * scale * elements,
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   backgroundColor = color(0, 0, 0);
-  frameRate(1);
+  //frameRate(4);
   for (var i = 0; i < vectorCount; i++) {
     vectors.push(createVector(
       gridWidth / 2 + int(random(-2,2)),
       gridHeight / 2 + int(random(-2,2))));
   }
-  for (x = 0; x < elements; x++) {
-    for (y = 0; y < elements; y++) {
-      //let vectorsCopy = vectors.map(v => {return v.copy()});
-      let pg = createGridElement(vectors, x + 1, y + 1, false);
-      image(pg, x * pg.width, y * pg.width, pg.width, pg.height);
-    }
+}
+
+function draw() {
+  let pg = createGridElement(vectors, x + 1, y + 1, false);
+  image(pg, x * pg.width, y * pg.width, pg.width, pg.height);
+  if (y === elements) {
+    y = 0;
+  } else if (x < elements) {
+    x++;
+  } else if (y < elements) {
+    x = 0;
+    y++;
+  } else {
+    y = 0;
+    x = 0;
+    console.log("CONDITION 4")
   }
-  noLoop();
+  delete pg;
+  console.log(JSON.stringify(vectors.map(v => { return {x: v.x, y: v.y}})))
 }
 
 function createGridElement(vectors, x, y, overlayGrid) {
@@ -39,8 +51,13 @@ function createGridElement(vectors, x, y, overlayGrid) {
 
   pg.fill(255)
   stroke(0, 255, 0);
-  vectors[0].add(int(random(-2,2)), 0);
-  vectors[1].add(0, int(random(-2,2)));
+  for (var i = 0; i < vectors.length; i++) {
+    vectors[i].add(int(random(-2,2)), int(random(-2,2)));
+    if (vectors[i].x < (0 - gridWidth /2))  vectors[i].x++;
+    if (vectors[i].x > (gridWidth * 1.5))  vectors[i].x--;
+    if (vectors[i].y < (0 - gridHeight /2))  vectors[i].y++;
+    if (vectors[i].y > (gridHeight * 1.5))  vectors[i].y--;
+  } 
   pg.beginShape();
   for (var i = 0; i < vectorCount; i++) {
     pg.vertex(vectors[i].x * scale, vectors[i].y * scale);
