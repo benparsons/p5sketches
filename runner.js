0e61a5952edf40259108554882e6d9d19f03682e
Ben Parsons <ben@bpulse.co.uk>
2020-07-04 10:00:04 +0100

    update runner to list project

--- runner.js
+++ runner.js
@@ -10,6 +10,26 @@ var fs = require('fs');
 
 //process.chdir(__dirname + "/..");
 
+
+app.get('/', function (req, res) {
+    let dir = fs.readdirSync(".");
+    let list = [];
+    dir.filter(function(file) {
+        var stat = fs.statSync(file);
+        if (!(stat && stat.isDirectory())) {
+            return;
+        }
+        if ([".git", "libraries", "node_modules", "new-blank", "images"].includes(file)) {
+            return;
+        }
+        list.push(file);
+    })
+    list = list.map(file => {return `<li>
+    <a href="${file}/">${file}</a>
+    </li>`})
+    res.send(list.join("\n"))
+});
+
 app.get('*', function (req, res) {
     var path = process.cwd() + req.path;
     if (fs.existsSync(path)) {
