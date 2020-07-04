42abcb435aba4139f19ec02cbaddd017f84003fc
Ben Parsons <ben@bpulse.co.uk>
2020-07-04 13:52:58 +0100

    use graphics element

--- 2020-07-04-grunberger1/sketch2.js
+++ 2020-07-04-grunberger1/sketch2.js
@@ -1,42 +1,57 @@
 let gridWidth = 8, gridHeight = 8;
-let scale = 100;
+let scale = 10;
 let vectors = [];
 let vectorCount = 3;
+let backgroundColor;
+let elements = 8;
 
-var canvasWidth = gridWidth * scale,
-  canvasHeight = gridHeight * scale;
+var canvasWidth = gridWidth * scale * elements,
+  canvasHeight = gridHeight * scale * elements;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  backgroundColor = color(0, 0, 0);
   frameRate(1);
   for (var i = 0; i < vectorCount; i++) {
     vectors.push(createVector(0, 0));
   }
+  drawElement();
+  noLoop();
 }
 
 function draw() {
-  background(0);
   stroke(0, 255, 0);
+  fill(255)
+  vectors[0].add(0, 1);
+  vectors[1].add(1, 0);
   beginShape();
   for (var i = 0; i < vectorCount; i++) {
     vertex(vectors[i].x * scale, vectors[i].y * scale);
   }
   endShape(CLOSE);
-  vectors[0].add(0, 1);
-  vectors[1].add(1, 0);
-  drawOverlay();
-  //noLoop();
+  //drawOverlay();
 }
 
-function drawOverlay() {
+function drawElement() {
+  let pgWidth = gridWidth * scale;
+  let pgHeight = gridWidth * scale;
+  pg = createGraphics(pgWidth, pgHeight);
+  pg.noStroke();
+  pg.fill(backgroundColor);
+  pg.rect(0, 0, pgWidth, pgHeight);
+  drawOverlay(pg);
+  image(pg, 0, 0, 100, 100);
+}
+
+function drawOverlay(pg) {
   for (var x = 0; x <= gridWidth; x++) {
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    line(x * scale, 0, x * scale, canvasHeight);
+    pg.stroke(255, 0, 0);
+    pg.strokeWeight(1);
+    pg.line(x * scale, 0, x * scale, canvasHeight);
   }
   for (var y = 0; y <= gridHeight; y++) {
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    line(0, y * scale, canvasWidth, y * scale);
+    pg.stroke(255, 0, 0);
+    pg.strokeWeight(1);
+    pg.line(0, y * scale, canvasWidth, y * scale);
   }
 }
