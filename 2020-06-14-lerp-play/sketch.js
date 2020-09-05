03674044ab41546ce0de1cee918066793090f197
Ben Parsons <ben@bpulse.co.uk>
2020-09-05 22:49:23 +0100

    improve render

--- 2020-06-14-lerp-play/sketch.js
+++ 2020-06-14-lerp-play/sketch.js
@@ -1,21 +1,28 @@
 var canvasWidth = 800,
   canvasHeight = 600;
 let colors = [];
-let count = 7;
+let count = 8;
 let steps = 20;
 let stepWidth = canvasWidth / steps;
+let blockHeight = canvasHeight  / count;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   colorMode(RGB);
   noStroke();
-  frameRate(1/2);
-  //noLoop();
+  render();
+  noLoop();
 }
 
-function draw() {
+function mousePressed(e) {
+  if (e.buttons !== 1) return;
+  render();
+}
+
+
+function render() {
   colors = [];
-  for (var i = 0; i < count; i++) {
+  for (var i = 0; i < count + 1; i++) {
     colors.push(
       color(
         int(random(256)),
@@ -24,13 +31,15 @@ function draw() {
     );
   }
 
-  for (var c = 0; c < count-1; c++) {
+  console.log(JSON.stringify(colors.map(c => c.levels)));
+
+  for (var c = 0; c < count; c++) {
     let from = colors[c];
     let to = colors[c+1];
     for (var s = 0; s < steps; s++) {
       let lerpVal = (1 / steps) * (steps - s);
       fill(lerpColor(from, to, lerpVal));
-      rect(steps*stepWidth - stepWidth*(s+1), c*100, stepWidth, 100);
+      rect(steps*stepWidth - stepWidth*(s+1), c * blockHeight, stepWidth, blockHeight);
     }
   }
 }
