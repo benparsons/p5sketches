9f7a92b81540dc81da899895dbd915f21aa52b48
Ben Parsons <ben@bpulse.co.uk>
2020-10-23 23:01:16 +0100

    use two layers

--- 2020-10-23/sketch.js
+++ 2020-10-23/sketch.js
@@ -5,26 +5,34 @@ let colorsWarm = ["511845", "900c3f", "c70039", "ff5733"]; // 179350
 let colorsCold = ["e7dfd5", "84a9ac", "3b6978", "204051"]; // 192310
 
 function setup() {
+  let pgBackground = createGraphics(canvasWidth, canvasHeight);
+  let pgForeground = createGraphics(canvasWidth, canvasHeight);
   createCanvas(canvasWidth, canvasHeight);
-  blendMode(HARD_LIGHT);
-  strokeWeight(10);
+  pgBackground.blendMode(HARD_LIGHT);
+  pgBackground.strokeWeight(10);
   for (let i = 0; i < 20000; i++) {
-    stroke(color("#" + colorsCold[int(random(0, colorsWarm.length))]))
-    let x = canvasWidth / 2;
-    x += randomGaussian() * canvasWidth / 4;
-    let y = canvasHeight / 2;
-    y += randomGaussian() * canvasHeight / 4;
-    point(x, y)
-  }
-  strokeWeight(100);
-  for (let i = 0; i < 80; i++) {
-    stroke(color("#" + colorsWarm[int(random(0, colorsWarm.length))]))
+    pgBackground.stroke(color("#" + colorsCold[int(random(0, colorsWarm.length))]))
     let x = canvasWidth / 2;
     x += randomGaussian() * canvasWidth / spreadFactor;
     let y = canvasHeight / 2;
     y += randomGaussian() * canvasHeight / spreadFactor;
-    point(x, y)
+    pgBackground.point(x, y)
   }
+  image(pgBackground, 0, 0);
+
+  //pgForeground.background(255, 255, 255);
+  pgForeground.blendMode(HARD_LIGHT);
+  pgForeground.strokeWeight(100);
+  for (let i = 0; i < 80; i++) {
+    pgForeground.stroke(color("#" + colorsWarm[int(random(0, colorsWarm.length))]))
+    let x = canvasWidth / 2;
+    x += randomGaussian() * canvasWidth / spreadFactor;
+    let y = canvasHeight / 2;
+    y += randomGaussian() * canvasHeight / spreadFactor;
+    pgForeground.point(x, y)
+  }
+  image(pgForeground, 0, 0);
+  
   noLoop();
 }
 
