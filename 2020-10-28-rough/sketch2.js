b484863311f5b6ee4ec582c9689065d5096e4df3
Ben Parsons <ben@bpulse.co.uk>
2020-10-28 18:59:37 +0000

    function

--- 2020-10-28-rough/sketch2.js
+++ 2020-10-28-rough/sketch2.js
@@ -6,24 +6,28 @@ var canvasWidth = 600,
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   let c = color("#4e89ae");
-  let result = roughEllipse(100, 100, c);
+  let result = roughEllipse(250, 250, c, 0.09);
   image(result, 0, 0)
 }
 
-function roughEllipse(w, h, c) {
+
+function draw() {
+}
+
+function roughEllipse(w, h, c, lr) {
   let pgMask = createGraphics(w, h);
   let pgTop = createGraphics(w, h);
-  let upper = lerpColor(color(255, 255, 255, 255), c, 0.9);
-  let lower = lerpColor(color(0, 0, 0, 255), c, 0.9);
+  let upper = lerpColor(color(255, 255, 255, 255), c, 1 - lr);
+  let lower = lerpColor(color(0, 0, 0, 255), c, 1 - lr);
   pgMask.noStroke();
   pgMask.fill(c);
   pgMask.ellipse(w / 2, h / 2, w, h);
 
-  noLoop();
   pgTop.fill(c);
   pgTop.ellipse(w / 2, h / 2, w, h);
   for (let i = 0; i < 1000; i++) {
-    pgTop.stroke(lerpColor(upper, lower, random(0, 1)));
+    let tempColor = lerpColor(upper, lower, random(0, 1));
+    pgTop.stroke(tempColor);
     pgTop.line(random(0, w), random(0, h), random(0, w), random(0, h))
   }
   var imgTop = createImage(pgTop.width,pgTop.height);
