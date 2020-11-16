e90855c953059ab5c8e8c0df6a3417369b0bbff1
Ben Parsons <ben@bpulse.co.uk>
2020-11-16 23:06:07 +0000

    multiple lines

--- 2020-11-16/sketch.js
+++ 2020-11-16/sketch.js
@@ -1,23 +1,33 @@
 var canvasWidth = 1200,
   canvasHeight = 600;
 let count = 10;
-
 // https://colorhunt.co/palette/206721
-let palette = ["0278ae", "51adcf", "a5ecd7", "e8ffc1"]; // 201413
+let palette = ["e8ffc1", "a5ecd7", "51adcf", "0278ae"];
+let rows = palette.length;
+
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  background("#f8ffd1")
   noLoop();
+  noStroke();
+  horizon(0);
+  horizon(1);
+  horizon(2);
+  horizon(3);
+}
+
+function horizon(n) {
   let points = [];
+  let baseY = canvasHeight / (rows - n + 0.5);
+  console.log(baseY);
   for (let i = 0; i < count + 1; i++) {
     let x = lerp(0, canvasWidth, i / count);
-    let y = randomGaussian() * 25 + canvasHeight / 2;
+    let y = randomGaussian() * 25 + baseY;
     let v = createVector(x, y);
-    console.log(i / count, v);
     points.push(v);
   }
-  console.log(points);
-  fill(color("#" + palette[0]))
+  fill(color("#" + palette[n]))
   beginShape();
     curveVertex(0, canvasHeight);
     curveVertex(0, canvasHeight * 2);
@@ -30,4 +40,3 @@ function setup() {
     curveVertex(canvasWidth, canvasHeight * 2);
   endShape(CLOSE);
 }
-
