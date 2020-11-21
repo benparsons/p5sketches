cbc932c86b5d1f90f74482ffcd1b881d321ff9b4
Ben Parsons <ben@bpulse.co.uk>
2020-11-21 19:08:10 +0000

    colorhunt samples

--- 2020-11-21b-colouring/sketch.js
+++ 2020-11-21b-colouring/sketch.js
@@ -3,11 +3,31 @@
 var canvasWidth = 1200,
   canvasHeight = 600;
 
-// https://colorhunt.co/palette/206721
 let palette = [];
 
 function setup() {
-  palette = [color("#e8ffc1"), color("#a5ecd7"), color("#51adcf"), color("#0278ae")];
+  // https://colorhunt.co/palette/206721
+  //palette = [color("#e8ffc1"), color("#a5ecd7"), color("#51adcf"), color("#0278ae")];
+
+  // https://colorhunt.co/palette/201413
+  //palette = [color("#4e89ae"), color("#43658b"), color("#ed6663"), color("#ffa372")];
+
+  //https://colorhunt.co/palette/161263
+  //palette = [color("#dcffcc"), color("#9fdfcd"), color("#baabda"), color("#d79abc")];
+
+  //https://colorhunt.co/palette/7642
+  //palette = [color("#f38181"), color("#fce38a"), color("#eaffd0"), color("#95e1d3")];
+
+  //https://colorhunt.co/palette/207237
+  palette = [color("#d789d7"), color("#9d65c9"), color("#5d54a4"), color("#2a3d66")];
+
+
+  //https://colorhunt.co/palette/207309
+  //palette = [color("#edcfa9"), color("#e89f71"), color("#d57149"), color("#aa4a30")];
+
+  //https://colorhunt.co/palette/161263
+  //palette = [color("#8ef6e4"), color("#9896f1"), color("#d59bf6"), color("#edb1f1")];
+
   createCanvas(canvasWidth, canvasHeight);
   noLoop();
   noStroke();
