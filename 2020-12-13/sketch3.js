9b3b03c06d2873288957e700ce84e98ccd135c50
Ben Parsons <ben@bpulse.co.uk>
2020-12-13 14:46:57 +0000

    use config object

--- 2020-12-13/sketch3.js
+++ 2020-12-13/sketch3.js
@@ -2,21 +2,35 @@ var canvasWidth = 600,
   canvasHeight = 600;
 let el;
 let el2;
-let gridSizeX = 80;
-let gridSizeY = 80;
 
-let sizeX = canvasWidth / gridSizeX;
-let sizeY = canvasHeight / gridSizeY;
+let sizeX;
+let sizeY;
 let panels = [];
 
+let config = {
+  gridSizeX: 80,
+  gridSizeY: 80,
+  gridColor: 230,
+  freshnessBoost: 20
+}
+
+// let config = {
+//   gridSizeX: 20,
+//   gridSizeY: 20,
+//   gridColor: 180,
+//   freshnessBoost: 5
+// }
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+
+  sizeX = canvasWidth / config.gridSizeX;
+  sizeY = canvasHeight / config.gridSizeY;
   //noStroke();
   el = new El({size:8});
   el2 = new El({size:8});
-  for (let x = 0; x < gridSizeX; x++) {
-    for (let y = 0; y < gridSizeY; y++) {
+  for (let x = 0; x < config.gridSizeX; x++) {
+    for (let y = 0; y < config.gridSizeY; y++) {
       panels.push(new Panel({x: x, y: y, freshness: 0}))
     }
   }
@@ -36,13 +50,13 @@ function draw() {
 
 function Panel ({x = 0, y = 0, freshness = 10} = {}) {
   this.render = (els) => {
-    stroke(180);
+    stroke(config.gridColor);
     els.forEach(el => {
       if (el.location.x < (x * sizeX + sizeX)
       && el.location.x > (x * sizeX) &&
       el.location.y < (y * sizeY + sizeY)
       && el.location.y > (y * sizeY)) {
-        freshness += 20;
+        freshness += config.freshnessBoost;
       }
     });
 
