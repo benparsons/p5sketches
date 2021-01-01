b912463cb10ffee1532fea783b6a1ad1d2ee3fa4
Ben Parsons <ben@bpulse.co.uk>
2021-01-01 22:58:23 +0000

    bridget riley?

---
+++ 2021-01-01-genuary1/sketch2.js
@@ -0,0 +1,29 @@
+var canvasWidth = 600,
+  canvasHeight = 600;
+let gridSize = 15;
+let blockSize = canvasWidth / gridSize;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  stroke(0);
+  noFill();
+  ellipseMode(CENTER)
+
+  let step = -10;
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      let sinStep = Math.sin(step / (gridSize * gridSize) * 2 * Math.PI) + 1;
+      console.log(step, sinStep, x, y);
+
+      for (let s = 0; s < blockSize / 2 * sinStep; s += 1) {
+        stroke(s * 5);
+        ellipse(
+          x * blockSize + blockSize / 2,
+          y * blockSize + blockSize / 2,
+          s, s);
+        //console.log("\t" + s);
+      }
+      step++;
+    }  
+  }
+}
