7fbd4911f69903ba8e334b039bed6a0d65fa3373
Ben Parsons <ben@bpulse.co.uk>
2021-01-02 18:12:59 +0000

    4x overlay

---
+++ 2021-01-01-genuary2-rule30/sketch2.js
@@ -0,0 +1,53 @@
+// genuray 2 / https://genuary2021.github.io/prompts / Rule 30
+
+// Rule 30: [left_cell XOR (central_cell OR right_cell)]
+// 0 ^ (0 || 0)
+
+var canvasWidth = 1200,
+  canvasHeight = canvasWidth / 8;
+let arr;
+let line = 0;
+let passes = 0;
+
+function setup() {
+  background(255);
+  createCanvas(canvasWidth, canvasHeight);
+  stroke(0);
+  arr = Array.apply(null, Array(canvasWidth)).map(function () { return 0; })
+  arr[canvasWidth/2] = 1;
+}
+
+function draw() {
+  drawLine();
+  calculateNext();
+  line++;
+  if (line > canvasHeight) {
+    //noLoop();
+    line = 0;
+    passes++;
+  }
+  if (passes >= 4) {
+    noLoop();
+  }
+}
+
+function drawLine() {
+  for (let x = 0; x < arr.length; x++) {
+    if (arr[x]) {
+      point(x, line);
+    }
+  }
+}
+
+function calculateNext() {
+  let nextArr = [];
+  for (let i in arr) {
+    i = parseInt(i, 10);
+    let l = arr[i - 1] || 0;
+    let c = arr[i];
+    let r = arr[i + 1] || 0;
+    nextArr[i] = l ^ (c || r);
+    //if (nextArr[i]) debugger;
+  }
+  arr = nextArr;
+}
