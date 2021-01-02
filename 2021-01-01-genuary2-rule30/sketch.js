0da711cf148599df5ecc025d9160b3209c53fd92
Ben Parsons <ben@bpulse.co.uk>
2021-01-02 18:00:51 +0000

    rule 30

---
+++ 2021-01-01-genuary2-rule30/sketch.js
@@ -0,0 +1,47 @@
+// genuray 2 / https://genuary2021.github.io/prompts / Rule 30
+
+// Rule 30: [left_cell XOR (central_cell OR right_cell)]
+// 0 ^ (0 || 0)
+
+var canvasWidth = 800,
+  canvasHeight = 600;
+let arr;
+let line = 0;
+
+function setup() {
+  background(255);
+  createCanvas(canvasWidth, canvasHeight);
+  stroke(0);
+  arr = Array.apply(null, Array(canvasWidth)).map(function () { return 0; })
+  arr[400] = 1;
+}
+
+function draw() {
+  drawLine();
+  calculateNext();
+  line++;
+  if (line > canvasHeight) {
+    noLoop();
+  }
+}
+
+function drawLine() {
+  for (let x = 0; x < arr.length; x++) {
+    if (arr[x]) {
+      point(x, line);
+    }
+  }
+}
+
+function calculateNext() {
+  let nextArr = [];
+  for (let i in arr) {
+    i = parseInt(i, 10);
+    let l = arr[i - 1] || 0;
+    let c = arr[i];
+    let r = arr[i + 1] || 0;
+    nextArr[i] = l ^ (c || r);
+    //if (nextArr[i]) debugger;
+  }
+  arr = nextArr;
+}
